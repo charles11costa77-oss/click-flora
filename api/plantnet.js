7cb59ceb5f7e23479c3a9150c2d0eb938ebc6ee1
charles11costa77-oss <charles11costa77@gmail.com>
2026-07-28 20:01:25 -0300

    Fix error handling in PlantNet API

--- api/plantnet.js
+++ api/plantnet.js
@@ -78,4 +78,4 @@ export default async function handler(req, res) {
     console.error('Erro PlantNet:', err);
     return res.status(500).json({ error: 'Erro interno ao identificar a planta' });
   }
-}
+} 
